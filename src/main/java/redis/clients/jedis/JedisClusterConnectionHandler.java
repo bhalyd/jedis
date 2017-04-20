536655545cae6530c396be35bdc8a300194d596d
Jason Blais <jason.blais@nielsen.com>
2017-04-20 11:06:23 -0400

    Fixes #1210 : Add parameters needed for ssl to HostAndPort and use them in JedisClusterConnectionHandler

--- src/main/java/redis/clients/jedis/JedisClusterConnectionHandler.java
+++ src/main/java/redis/clients/jedis/JedisClusterConnectionHandler.java
@@ -8,64 +8,82 @@ import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
 
 import redis.clients.jedis.exceptions.JedisConnectionException;
 
-public abstract class JedisClusterConnectionHandler implements Closeable {
-  protected final JedisClusterInfoCache cache;
+public abstract class JedisClusterConnectionHandler implements Closeable
+{
+	protected final JedisClusterInfoCache cache;
 
-  public JedisClusterConnectionHandler(Set<HostAndPort> nodes,
-                                       final GenericObjectPoolConfig poolConfig, int connectionTimeout, int soTimeout, String password) {
-    this(nodes, poolConfig, connectionTimeout, soTimeout, password, null);
-  }
+	public JedisClusterConnectionHandler(Set<HostAndPort> nodes,
+			final GenericObjectPoolConfig poolConfig, int connectionTimeout, int soTimeout, String password)
+	{
+		this(nodes, poolConfig, connectionTimeout, soTimeout, password, null);
+	}
 
-  public JedisClusterConnectionHandler(Set<HostAndPort> nodes,
-          final GenericObjectPoolConfig poolConfig, int connectionTimeout, int soTimeout, String password, String clientName) {
-    this.cache = new JedisClusterInfoCache(poolConfig, connectionTimeout, soTimeout, password, clientName);
-    initializeSlotsCache(nodes, poolConfig, password, clientName);
-}
+	public JedisClusterConnectionHandler(Set<HostAndPort> nodes,
+			final GenericObjectPoolConfig poolConfig, int connectionTimeout, int soTimeout, String password, String clientName)
+	{
+		this.cache = new JedisClusterInfoCache(poolConfig, connectionTimeout, soTimeout, password, clientName);
+		initializeSlotsCache(nodes, poolConfig, password, clientName);
+	}
 
-  abstract Jedis getConnection();
+	abstract Jedis getConnection();
 
-  abstract Jedis getConnectionFromSlot(int slot);
+	abstract Jedis getConnectionFromSlot(int slot);
 
-  public Jedis getConnectionFromNode(HostAndPort node) {
-    return cache.setupNodeIfNotExist(node).getResource();
-  }
-  
-  public Map<String, JedisPool> getNodes() {
-    return cache.getNodes();
-  }
+	public Jedis getConnectionFromNode(HostAndPort node)
+	{
+		return cache.setupNodeIfNotExist(node).getResource();
+	}
 
-  private void initializeSlotsCache(Set<HostAndPort> startNodes, GenericObjectPoolConfig poolConfig, String password, String clientName) {
-    for (HostAndPort hostAndPort : startNodes) {
-      Jedis jedis = new Jedis(hostAndPort.getHost(), hostAndPort.getPort());
-      try {
-        if (password != null) {
-          jedis.auth(password);
-        }
-        if (clientName != null) {
-          jedis.clientSetname(clientName);
-        }
-        cache.discoverClusterNodesAndSlots(jedis);
-        break;
-      } catch (JedisConnectionException e) {
-        // try next nodes
-      } finally {
-        if (jedis != null) {
-          jedis.close();
-        }
-      }
-    }
-  }
+	public Map<String, JedisPool> getNodes()
+	{
+		return cache.getNodes();
+	}
 
-  public void renewSlotCache() {
-    cache.renewClusterSlots(null);
-  }
+	private void initializeSlotsCache(Set<HostAndPort> startNodes, GenericObjectPoolConfig poolConfig, String password, String clientName)
+	{
+		for (HostAndPort hostAndPort : startNodes)
+		{
 
-  public void renewSlotCache(Jedis jedis) {
-    cache.renewClusterSlots(jedis);
-  }
+			Jedis jedis = new Jedis(hostAndPort.getHost(), hostAndPort.getPort(), hostAndPort.isSsl(), hostAndPort.getSslSocketFactory(), hostAndPort.getSslParameters(), hostAndPort.getHostnameVerifier());
 
-  @Override
-  public void close() {
-    cache.reset();
-  }
+			try
+			{
+				if (password != null)
+				{
+					jedis.auth(password);
+				}
+				if (clientName != null)
+				{
+					jedis.clientSetname(clientName);
+				}
+				cache.discoverClusterNodesAndSlots(jedis);
+				break;
+			} catch (JedisConnectionException e)
+			{
+				// try next nodes
+			} finally
+			{
+				if (jedis != null)
+				{
+					jedis.close();
+				}
+			}
+		}
+	}
+
+	public void renewSlotCache()
+	{
+		cache.renewClusterSlots(null);
+	}
+
+	public void renewSlotCache(Jedis jedis)
+	{
+		cache.renewClusterSlots(jedis);
+	}
+
+	@Override
+	public void close()
+	{
+		cache.reset();
+	}
 }
